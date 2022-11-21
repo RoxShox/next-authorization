516ab21980bf7eb45ab93eebe2d8f25d71f35f57
RoxShox <19romale77@mail.ru>
2022-11-21 16:50:17 +0300

    update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Footer.module.scss'
+const Footer = () => {
+	return (
+		<div className={styles.footer}>
+			<p>Coopyright 2022 - by RoxShox & S1ma</p>
+		</div>
+	)
+}
+
+export default Footer
