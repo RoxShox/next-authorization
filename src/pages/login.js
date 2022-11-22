69faa9554c6ea14e088b3ddf5cb59b479e77a175
Daniil Simanovich <s1mahochukvas@vk.com>
2022-11-22 12:15:05 +0300

    AuthForm modified

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import AuthForm from '../components/AuthForm/AuthForm'
 
 const login = () => {
-	return <div></div>
+	return (
+		<div style={{display: 'flex', justifyContent: 'center'}}>
+			<AuthForm type='login' />
+		</div>
+	)
 }
 
 export default login
