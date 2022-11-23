6e38d53eadf06adeab0dc013ee92a9cfbca258c6
RoxShox <19romale77@mail.ru>
2022-11-23 17:13:10 +0300

    serv

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { registerValidation, loginValidation } from './validations/validations.js'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import { checkAuth, handleValidationErrors } from './utils/index.js'
+import * as UserController from './controllers/UserController.js'
+
+mongoose
+	.connect(
+		'mongodb+srv://admin:12345@cluster0.m8s97xd.mongodb.net/blog?retryWrites=true&w=majority',
+	)
+	.then(() => {
+		console.log('DB ok')
+	})
+	.catch(err => console.log('DB ERRor', err))
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/auth/me', checkAuth, UserController.getMe)
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
+
+app.listen(4444, err => {
+	if (err) {
+		console.log('Error')
+	}
+	console.log('server ok')
+})
