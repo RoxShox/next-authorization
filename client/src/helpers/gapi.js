fb47667e07c9c4d60079617d1655ca1f90019eff
Daniil Simanovich <s1mahochukvas@vk.com>
2022-11-24 15:17:43 +0300

    {google} jwt token decode

--- client/src/helpers/gapi.js
+++ client/src/helpers/gapi.js
@@ -1,7 +1,8 @@
-
+import jwt_decode from 'jwt-decode'
 export default function gapi(google, btnRef) {
     function googleCallback(res) {
-        console.log(`Google res: ${res}`)
+        const user = jwt_decode(res.credential)
+        console.log(user)
     }
     google.accounts.id.initialize({
         client_id: "480864807133-r37dumtbb7vcgj62pcs29833phk2opck.apps.googleusercontent.com",
